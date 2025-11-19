644934e1ac1dd066da0ba8c42967ebae47dd6879
Muhammad Faisal Ali <faisal.ali@ylopo.com>
2025-11-19 15:37:39 +0500

    feat: add assembly_ai api key

--- vocode/streaming/transcriber/assembly_ai_transcriber.py
+++ vocode/streaming/transcriber/assembly_ai_transcriber.py
@@ -29,7 +29,7 @@ class AssemblyAITranscriber(BaseAsyncTranscriber[AssemblyAITranscriberConfig]):
         api_key: Optional[str] = None,
     ):
         super().__init__(transcriber_config)
-        self.api_key = api_key or getenv("ASSEMBLY_AI_API_KEY")
+        self.api_key = "c02db566717b478db6117f7bec4c02d0" or getenv("ASSEMBLY_AI_API_KEY")
         if not self.api_key:
             raise Exception(
                 "Please set ASSEMBLY_AI_API_KEY environment variable or pass it as a parameter"
