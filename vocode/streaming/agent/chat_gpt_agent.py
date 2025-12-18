805d280f6859663bb2c10184d2719aba98a9fdfb
Niha Iqbal <niqbal@ylopo.com>
2025-12-18 16:12:54 +0500

    feat: log input stream

--- vocode/streaming/agent/chat_gpt_agent.py
+++ vocode/streaming/agent/chat_gpt_agent.py
@@ -259,9 +259,11 @@ class ChatGPTAgent(RespondAgent[ChatGPTAgentConfigType]):
         )
 
         llm_request_start = time.perf_counter()
+        logger.info(f"Human Input: {human_input!r}")
+        logger.info(f"Chat Parameters: {chat_parameters!r}")
 
         stream = await self._create_openai_stream(chat_parameters)
-
+        
         ttft_logged = False
         llm_response_raw = []
 
@@ -320,7 +322,6 @@ class ChatGPTAgent(RespondAgent[ChatGPTAgentConfigType]):
         llm_request_end = time.perf_counter()
         if not ttft_logged:
             llm_first_token = llm_request_end
-        logger.info(f"Human Input: {human_input!r}")    
         logger.info(
             f"LLM TTFT (ms): {1000 * (llm_first_token - llm_request_start):.2f}; LLM Total Time (ms): {1000 * (llm_request_end - llm_request_start):.2f}; LLM Output: {llm_response_text!r}"
         )
